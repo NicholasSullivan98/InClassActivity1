a94a41e172f21016ef6125c36b78f677e534bbe0
NicholasSullivan98 <78221412+NicholasSullivan98@users.noreply.github.com>
2021-02-05 11:53:54 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -12,6 +12,7 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card c1 = new Card();
+       System.out.println("Suit: " + c1.getSuit() + "Value: " + c1.getValue());
     }
     
 }
