534b375bebc646a87f6f78cc83f86b4d38001cc7
NicholasSullivan <sullnich@sheridancollege.ca>
2021-02-05 11:48:57 -0500

    Added First Object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -8,6 +8,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * 
+ * Edited by Nicholas Sullivan
+ * Student Number: 991612414
  */
 public class Card {
 
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,11 +4,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * 
+ * Edited by Nicholas Sullivan
+ * Student Number: 991612414
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
     }
     
 }
